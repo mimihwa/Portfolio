bd7473218449acf8bb324ea8299a8c5585400833
mimihwa <wmfskf@naver.com>
2023-03-14 02:47:21 +0900

    Start모션ing, Enter연결ing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from "react";
-import Start from './page/Start';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './page/Home.js'
+import Start from './page/Start.js'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Start />
+        <Routes>
+            <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/page/Start.js
+++ src/page/Start.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { gsap, Sine } from "gsap";
 import '../component/style/Start.scss';
-import  {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Start = (props) => {
-
+const Start = () => {
     console.clear();
-    const randomX = random(-400, 400);
-    const randomY = random(-200, 200);
+    const randomX = random(-700, 700);
+    const randomY = random(-500, 500);
     const randomDelay = random(0, 50);
-    const randomTime = random(7, 14);
-    const randomTime2 = random(6, 7);
+    const randomTime = random(6, 12);
+    const randomTime2 = random(5, 6);
     const randomAngle = random(-30, 150);
-
+ 
     const blurs = gsap.utils.toArray(".blur");
     blurs.forEach((blur) => {
         gsap.set(blur, {
             x: randomX(-1),
             y: randomX(1),
-            repeat: Infinity,
+            repeat: -1,
+            instantRender: false,
             rotation: randomAngle(-1)
         });
-        moveX(blur, 2);
-        moveY(blur, -2);
+        moveX(blur, 1);
+        moveY(blur, -1);
         rotate(blur, 1);
     });
-
+    
     function rotate(target, direction) {
         gsap.to(target, randomTime2(), {
             rotation: randomAngle(direction),
@@ -53,26 +53,26 @@ const Start = (props) => {
             onCompleteParams: [target, direction * -1]
         });
     }
+
     function random(min, max) {
         const delta = max - min;
         return (direction = 1) => (min + delta * Math.random()) * direction;
-
     }
+    
 
     
     return (
-        <div id='start'>
-            <div className='blurWrap'>
-                <div className="blur big"></div>
-                <div className="blur middle"></div>
-                <div className="blur small"></div>
+            <div id='start'>
+                <div className='blurWrap'>
+                    <div className="blur big"></div>
+                    <div className="blur middle"></div>
+                    <div className="blur small"></div>
+                </div>
+                <div className='title'>
+                    <h1>Welcome to the<span>MIHWA WORLD</span></h1>
+                    <Link to='/Home'><button>ENTER</button></Link>
+                </div >
             </div>
-            <div className='title'>
-                <h1>Welcome to the<span>MIHWA WORLD</span></h1>
-                <button>ENTER</button>
-                {/* <Link to='/Home'>ENTER</Link> */}
-            </div >
-        </div>
     );
 };
 
