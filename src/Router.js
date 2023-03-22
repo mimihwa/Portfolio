57f682b8fcdbc2c5968df0384261dc4ae8b231f9
mimihwa <wmfskf@naver.com>
2023-03-22 11:20:07 +0900

    Router 수정

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { HashRouter, Routes, Route} from 'react-router-dom';
 import About from './page/About.js';
 import Project from './page/Project.js';
 import Start from './page/Start.js';
@@ -8,14 +8,14 @@ import Comment from './page/Comment.js';
 function Router() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
             <Route path="/" element={<Start />} />
             <Route path="/About" element={<About />}/>
             <Route path="/Project" element={<Project />}/>
             <Route path="/Comment" element={<Comment />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
